04665cef4a67f60a7f5e85a16df389091fe348dd
James Muriuki <jamesmuriukimbuthia@gmail.com>
2016-12-01 16:24:55 +0300

    Added js effect to hide and show when form is submitted

--- ip2/js/script.js
+++ ip2/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+$("form#subscribe").submit(function  () {
+	var input=$("#emailInput").val();
+	$(".unsubscribed").hide();
+	$(".subscribed").show();
 
+
+	event.preventDefault();
+	// body... 
+})
 
 
 
