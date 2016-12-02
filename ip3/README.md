aefd5acf8e3d5190b9919263adbf97b562d58008
James Muriuki <jamesmuriukimbuthia@gmail.com>
2016-12-02 17:40:26 +0300

    this commit will add finished ping pong application

---
+++ ip3/README.md
@@ -0,0 +1,10 @@
+##Ping Pong application
+### Introduction
+This application takes the users number input and displays it in a range from 1 to the number
+It then looks if the number in a range is divisible by 3,5 15 and replaces those numbers with ping pong and Ping pong respectively
+###How it works
+- user inputs a number from 1-100
+- the program checks if it is a valid number and is in the range
+- a loop is created to keep on adding one from 1 till it reaches the inputed number
+- A series of if else statements check divisibility of numbers by 3,5,15 and replaces them with a new value
+- the range is placed in list items to be displayed on the html file
---
+++ ip3/js/script.js
@@ -0,0 +1,70 @@
+	//Business Logic
+	//var inNumber=Number(prompt("Enter a Number"));
+
+	//ui logic
+
+	$(document).ready(function(){
+
+
+		//name submission
+		$("#inName").submit(function  () {
+		var name=$("#nameIn").val();
+		$(".outName").text(name);
+		$(".displayName").show();
+		$("#inName").hide();
+
+		event.preventDefault();
+
+
+		})
+		$("#numberInput").submit(function () {
+		/* body... */
+		// number checked if= valid less than 100 
+
+			var inNumber=$("#numberIn").val();
+			if(!isNaN(inNumber) && inNumber > 100){
+				$(".lists").append("Enter a valid number from 1-100");
+
+			}
+
+			for(var i=1; i<=inNumber; i++){
+
+				if ( i == 15|| i % 15 === 0) {
+
+					//console.log("Ping Pong");	
+					$(".lists").append("<li>"+"ping pong"+"</li>");
+				
+				}else if (i % 5 ===0) {
+					$(".lists").append("<li>"+" pong"+"</li>");
+			
+					
+
+				}else if (i % 3 ==0) {
+
+					$(".lists").append("<li>"+"ping"+"</li>");
+					//console.log("Ping")
+					
+				}else {
+					$(".lists").append("<li>"+i+"</li>");
+					//console.log(i);
+					
+				}
+
+					
+				$(".lists").show();
+
+
+			}
+			
+
+			event.preventDefault();
+
+	})
+
+
+
+
+
+
+
+	});
