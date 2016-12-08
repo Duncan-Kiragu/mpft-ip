02d30f2d3f569a3049f998ebb42eadcec32255f6
James Muriuki <jamesmuriukimbuthia@gmail.com>
2016-12-09 02:36:07 +0300

    deleted gh-pages

--- ip2/js/script.js
+++ ip2/js/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
-$("form#subscribe").submit(function  () {
-	var input=$("#emailInput").val();
-	$(".unsubscribed").hide();
-	$(".subscribed").show();
+	$("form#subscribe").submit(function  () {
+		var input=$("#emailInput").val();
+		$(".unsubscribed").hide();
+		$(".subscribed").show();
 
 
-	event.preventDefault();
-	// body... 
-})
+		event.preventDefault();
+		// body... 
+	});
+	$(".hamberger-menu").click(function(){
+    document.getElementById("myNav").style.width = "100%";
+	});
+	
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+	$(".closebtn").click(function() {
+    document.getElementById("myNav").style.width = "0%";
+	});
 
 
 
